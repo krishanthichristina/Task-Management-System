2ff93789c79008976325c67e2f6ffe8edfee615b
Krishanthi Christina <115383422+krishanthichristina@users.noreply.github.com>
2026-02-19 23:34:44 +0530

    project created

---
+++ TaskMannagement/src/main/java/org/example/taskmannagement/TaskMannagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.taskmannagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMannagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMannagementApplication.class, args);
+	}
+
+}
